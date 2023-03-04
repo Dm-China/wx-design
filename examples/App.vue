420011f10cab6f7576f2c844ab89a102a11375b5
Dmhb <189838678@qq.com>
2023-03-04 17:27:32 +0800

    init project

---
+++ examples/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <WxSelect />
+  </div>
+</template>
+
+<script>
+import WxSelect from "../packages/select/src/Select.vue";
+
+export default {
+  name: "App",
+  components: { WxSelect: WxSelect },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
